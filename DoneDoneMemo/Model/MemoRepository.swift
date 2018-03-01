d7d42e0a61cd4c45a235e5eed414ddb6b9c84c65
Yuki Sumida <y.sumida.0201@gmail.com>
2018-03-01 23:01:34 +0900

    空のMemoRepositoryの追加

---
+++ DoneDoneMemo/Model/MemoRepository.swift
@@ -0,0 +1,10 @@
+//
+//  MemoRepository.swift
+//  DoneDoneMemo
+//
+//  Created by Yuki Sumida on 2018/03/01.
+//  Copyright © 2018年 Yuki Sumida. All rights reserved.
+//
+
+class MemoRipository {
+}
