2f29efcf9223803aadd4a1b4ecabfd38a485624a
Yuki Sumida <y.sumida.0201@gmail.com>
2018-03-08 22:41:01 +0900

    タスク用セルのDoneとEditのアクションを仮実装

--- DoneDoneMemo/View/View/TaskCell.swift
+++ DoneDoneMemo/View/View/TaskCell.swift
@@ -2,12 +2,30 @@ import UIKit
 
 class TaskCell: UITableViewCell {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneButton: NSLayoutConstraint!
+    @IBOutlet weak var editButton: UIButton!
+
+    var done: Bool = false {
+        didSet {
+            editButton.isEnabled = !done
+            // TODO 取り消し線とかチェックマークとか
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        textField.isUserInteractionEnabled = false // いらない？
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+
+    @IBAction func tapDoneButton(_ sender: Any) {
+        done = !done
+    }
+
+    @IBAction func tapEditButton(_ sender: Any) {
+        textField.becomeFirstResponder()
     }
 }
