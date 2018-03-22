707fdc7b1f0b43619117a4153780038c844629a6
Yuki Sumida <y.sumida.0201@gmail.com>
2018-03-22 22:39:22 +0900

    セルタップでもdoneになるようにする

--- DoneDoneMemo/View/ViewController/MemoContentViewController.swift
+++ DoneDoneMemo/View/ViewController/MemoContentViewController.swift
@@ -53,6 +53,13 @@ extension MemoContentViewController: UITableViewDataSource {
 }
 
 extension MemoContentViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? TaskCell else { return }
+        cell.setSelected(true, animated: true)
+        cell.toggleTask()
+        cell.setSelected(false, animated: true)
+        viewModel.toggleDone(at: indexPath.row)
+    }
     func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
         return "削除"
     }
