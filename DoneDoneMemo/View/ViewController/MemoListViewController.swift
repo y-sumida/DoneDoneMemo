0bf04e0c9a2ff4001193f9046b139993c77edb10
Yuki Sumida <y.sumida.0201@gmail.com>
2018-04-05 22:47:21 +0900

    セル削除をbeginUpdates()とendUpdates()で挟む

--- DoneDoneMemo/View/ViewController/MemoListViewController.swift
+++ DoneDoneMemo/View/ViewController/MemoListViewController.swift
@@ -61,8 +61,10 @@ extension MemoListViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete, viewModel.numberOfMemos > indexPath.row {
+            tableView.beginUpdates()
             viewModel.deleteMemo(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.endUpdates()
         }
     }
 }
