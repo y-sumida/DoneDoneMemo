9a0970a20428054fef4778e3e35fbfbab4a60990
Yuki Sumida <y.sumida.0201@gmail.com>
2018-05-07 22:23:30 +0900

    メモ一覧のナビゲーションタイトルを表示する

--- DoneDoneMemo/View/ViewController/MemoCollectionViewController.swift
+++ DoneDoneMemo/View/ViewController/MemoCollectionViewController.swift
@@ -8,11 +8,18 @@ class MemoCollectionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [
+            NSAttributedStringKey.font: UIFont.systemFont(ofSize: 30, weight: .black),
+            NSAttributedStringKey.foregroundColor: UIColor.black
+        ]
+        navigationItem.title = "メモ一覧"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 }
+
 extension MemoCollectionViewController: StoryboardInstantiatable {
 }
