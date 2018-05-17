44b96278151862fd386893e111e5f24c76f2dd9f
Yuki Sumida <y.sumida.0201@gmail.com>
2018-05-17 21:30:44 +0900

    メモ設定画面にタイトルを表示する

--- DoneDoneMemo/View/ViewController/MemoSettingsViewController.swift
+++ DoneDoneMemo/View/ViewController/MemoSettingsViewController.swift
@@ -7,10 +7,15 @@ final class MemoSettingsViewController: UIViewController {
     typealias Dependency = MemoSettingsViewModel
     private var viewModel: MemoSettingsViewModel!
 
+    @IBOutlet private weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = viewModel.title + "の設定"
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +26,28 @@ final class MemoSettingsViewController: UIViewController {
 extension MemoSettingsViewController: StoryboardInstantiatable {
     func inject(_ dependency: MemoSettingsViewModel) {
         viewModel = dependency
+    }
+}
+
+
+extension MemoSettingsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO 専用のセルつくる
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
+
+        cell.textLabel?.text = viewModel.title
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "メモのタイトル"
     }
 }
