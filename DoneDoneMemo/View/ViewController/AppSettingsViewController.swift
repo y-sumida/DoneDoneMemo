c40fbafdefaee1974c36e5bdc8bb840c6226c435
Yuki Sumida <y.sumida.0201@gmail.com>
2018-05-29 22:48:18 +0900

    ライセンス用plistを読み出す

--- DoneDoneMemo/View/ViewController/AppSettingsViewController.swift
+++ DoneDoneMemo/View/ViewController/AppSettingsViewController.swift
@@ -8,10 +8,39 @@ final class AppSettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // TODO tableViewに表示する
+        _ = readLicenses()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    // TODO Modelに移動する
+    private func readLicenses() -> [String: String] {
+        guard let path: URL = Bundle.main.url(forResource: "Settings.bundle/com.mono0926.LicensePlist", withExtension: "plist") else {
+            return [:]
+        }
+
+        do {
+            let data: Data = try Data(contentsOf: path)
+            let properties = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any]
+
+            var licenses: [String: String] = [:]
+            if let preference = properties?["PreferenceSpecifiers"] as? [[String: Any]] {
+                preference.forEach {
+                    if let title = $0["Title"] as? String,
+                        let path = $0["File"] as? String {
+                        licenses[title] = path
+                    }
+                }
+            }
+            dump(licenses)
+            return licenses
+        } catch {
+            return [:]
+        }
     }
 }
 
