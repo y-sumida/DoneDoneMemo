b7a233759ee58c56fdb96b38a4289897747ce1b1
Yuki Sumida <y.sumida.0201@gmail.com>
2018-06-17 22:34:23 +0900

    入力欄に期限を表示する

--- DonePad/View/View/KeyboardTextView.swift
+++ DonePad/View/View/KeyboardTextView.swift
@@ -7,6 +7,8 @@ final class KeyboardTextView: UIView {
     @IBOutlet private weak var textView: UITextView!
     @IBOutlet private weak var timerButton: UIButton!
     @IBOutlet private weak var sendButton: UIButton!
+    @IBOutlet weak var deadlineLabel: UILabel!
+    @IBOutlet weak var deadlineClearButton: UIButton!
     weak var delegate: UITextViewDelegate? {
         didSet {
             textView.delegate = delegate
@@ -33,15 +35,25 @@ final class KeyboardTextView: UIView {
         return CGSize(width: self.bounds.width, height: textSize.height + 16)
     }
 
-    func showKeyboard(title: String = "") {
+    func showKeyboard(title: String = "", deadline: Date? = nil) {
         textView.text = title
         invalidateIntrinsicContentSize()
         timerButton.isEnabled = true
+
+        if let date = deadline {
+            deadlineLabel.text = date.description
+            deadlineClearButton.isHidden = false
+        } else {
+            deadlineLabel.text = "期限なし"
+            deadlineClearButton.isHidden = true
+        }
         textView.becomeFirstResponder()
     }
 
     func hideKeyboard() {
         textView.text = ""
+        deadlineLabel.text = "期限なし"
+        deadlineClearButton.isHidden = true
         invalidateIntrinsicContentSize()
         textView.resignFirstResponder()
     }
@@ -62,5 +74,8 @@ extension KeyboardTextView: NibInstantiatable {
     func inject(_ dependency: Void) {
         textView.layer.cornerRadius = 4
         sendButton.layer.cornerRadius = 18
+        deadlineClearButton.layer.cornerRadius = 10
+        deadlineLabel.text = "期限なし"
+        deadlineClearButton.isHidden = true
     }
 }
