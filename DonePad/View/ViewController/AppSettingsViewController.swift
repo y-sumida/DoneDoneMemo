9c54335d199172278327b768fc14b83d05760064
Yuki Sumida <y.sumida.0201@gmail.com>
2018-06-23 22:34:28 +0900

    アプリの設定項目を上に表示する

--- DonePad/View/ViewController/AppSettingsViewController.swift
+++ DonePad/View/ViewController/AppSettingsViewController.swift
@@ -94,18 +94,18 @@ extension AppSettingsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch (indexPath.section, indexPath.row) {
         case (0, 0):
-            let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
-            let cell = AppSettingsVersionCell.dequeue(from: tableView, for: indexPath, with: version)
-            return cell
-        case (0, 1):
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-            cell.textLabel?.text = viewModel.title(for: indexPath)
-            cell.accessoryType = .disclosureIndicator
-            return cell
-        case (1, 0):
             let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
             cell.textLabel?.text = viewModel.title(for: indexPath)
             cell.detailTextLabel?.text = viewModel.allowPush.value ? "On" : "Off"
+            return cell
+        case (1, 0):
+            let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
+            let cell = AppSettingsVersionCell.dequeue(from: tableView, for: indexPath, with: version)
+            return cell
+        case (1, 1):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = viewModel.title(for: indexPath)
+            cell.accessoryType = .disclosureIndicator
             return cell
         default:
             return UITableViewCell()
@@ -116,16 +116,16 @@ extension AppSettingsViewController: UITableViewDataSource {
 extension AppSettingsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch (indexPath.section, indexPath.row) {
-        case (0, 1):
+        case (0, 0):
+            guard let url = URL(string: "App-Prefs:root=NOTIFICATIONS_ID&path=" + (Bundle.main.bundleIdentifier ?? "")) else { return }
+            UIApplication.shared.open(url)
+        case (1, 1):
             let vc = LicensesViewController(with: Void())
             let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: nil)
             backButton.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 17, weight: .light)], for: .normal)
             backButton.tintColor = UIColor.black
             navigationItem.backBarButtonItem = backButton
             navigationController?.pushViewController(vc, animated: true)
-        case (1, 0):
-            guard let url = URL(string: "App-Prefs:root=NOTIFICATIONS_ID&path=" + (Bundle.main.bundleIdentifier ?? "")) else { return }
-            UIApplication.shared.open(url)
         default:
             break
         }
--- DonePad/ViewModel/AppSettingsViewModel.swift
+++ DonePad/ViewModel/AppSettingsViewModel.swift
@@ -21,7 +21,7 @@ class AppSettingsViewModel {
 
     func numberOfRowsInSection(section: Int) -> Int {
         switch section {
-        case 0: return 2
+        case 1: return 2
         default: return 1
         }
     }
@@ -29,11 +29,11 @@ class AppSettingsViewModel {
     func title(for index: IndexPath) -> String {
         switch (index.section, index.row) {
         case (0, 0):
-            return "version"
-        case (0, 1):
-            return "License"
-        case (1, 0):
             return "Alarm"
+        case (1, 0):
+            return "version"
+        case (1, 1):
+            return "License"
         default:
             return ""
         }
@@ -41,8 +41,8 @@ class AppSettingsViewModel {
 
     func titleForHeaderInsection(section: Int) -> String? {
         switch section {
-        case 0: return "DonPad"
-        case 1: return "Settings"
+        case 0: return "Settings"
+        case 1: return "DonPad"
         default: return nil
         }
     }
