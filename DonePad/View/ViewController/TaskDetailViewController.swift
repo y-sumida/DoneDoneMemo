d37762ab94de40bed90c10f30a486ad761c790d3
Yuki Sumida <y.sumida.0201@gmail.com>
2018-07-05 22:48:53 +0900

    タスク詳細のナビゲーションを仮実装

--- DonePad/View/ViewController/TaskDetailViewController.swift
+++ DonePad/View/ViewController/TaskDetailViewController.swift
@@ -5,6 +5,25 @@ import InstantiateStandard
 final class TaskDetailViewController: UIViewController {
     // StoryboardInstantiatable
     typealias Dependency = Void
+
+    override func viewDidLoad() {
+        navigationItem.title = "詳細"
+        let closeButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(self.close))
+        closeButton.tintColor = UIColor.black
+        closeButton.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 17, weight: .light)], for: .normal)
+        navigationItem.leftBarButtonItem = closeButton
+        let saveButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(self.save))
+        saveButton.tintColor = UIColor.black
+        saveButton.setTitleTextAttributes([.foregroundColor: UIColor.lightGray, .font: UIFont.systemFont(ofSize: 17, weight: .light)], for: .disabled)
+        navigationItem.rightBarButtonItem = saveButton
+    }
+
+    @objc func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @objc func save() {
+        close()
+    }
 }
 
 extension TaskDetailViewController: StoryboardInstantiatable {
