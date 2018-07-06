b1b4a46267884ca56a8a4f2188549a931f62621d
Yuki Sumida <y.sumida.0201@gmail.com>
2018-07-06 22:12:17 +0900

    セルに引き渡すVariableを追加

--- DonePad/ViewModel/TaskViewModel.swift
+++ DonePad/ViewModel/TaskViewModel.swift
@@ -1,9 +1,13 @@
 import Foundation
+import RxSwift
 
 struct TaskViewModel {
     private var task: Task!
 
+    var title: Variable<String> = Variable("")
+
     init(from task: Task) {
         self.task = task
+        title.value = task.title
     }
 }
